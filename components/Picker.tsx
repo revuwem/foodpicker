248a9dbe277dc272e00b6cf4a20539ed25a8d2e6
Carina <revuwem@gmail.com>
2022-08-06 02:19:55 +0400

    style: fix Picker style

--- components/Picker.tsx
+++ components/Picker.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const Picker = () => {
   return (
-    <div className="max-w-xl flex flex-col items-center">
+    <div className="max-w-xl p-4 flex flex-col items-center">
       <Image
         src={pastaImg}
         width="600px"
@@ -13,7 +13,7 @@ const Picker = () => {
         placeholder="blur"
         className="mb-8"
       />
-      <h3 className="font-normal text-2xl md:text-3xl mb-12 md:mb-16">
+      <h3 className="font-normal text-2xl md:text-3xl mb-10 md:mb-14">
         Are you hungry?
       </h3>
       <Button onClick={(e) => console.log("pick random food")}>
