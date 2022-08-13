f3f48d6c3e40dbc87a3bb15ee9673c81de366e69
Carina <revuwem@gmail.com>
2022-08-14 00:24:24 +0400

    feat: change /random page layout

--- pages/random.tsx
+++ pages/random.tsx
@@ -2,6 +2,11 @@ import { NextPage } from "next";
 import RecipeCard from "../components/RecipeCard";
 import useSWR from "swr";
 import { randomRecipeFetcher } from "../utils/fetcher";
+import Container from "../components/Container";
+import FlexCenter from "../components/FlexCenter";
+import Button from "../components/Button";
+import Router from "next/router";
+import Message from "../components/Message";
 
 const Random: NextPage = () => {
   const { data, error } = useSWR(
@@ -9,10 +14,45 @@ const Random: NextPage = () => {
     randomRecipeFetcher
   );
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  if (error) {
+    return (
+      <Container>
+        <FlexCenter>
+          <div className="grid gap-5">
+            <Message msg="Sorry, we don't have any recipes yet" />
+          </div>
+        </FlexCenter>
+      </Container>
+    );
+  }
 
-  return <RecipeCard data={data} />;
+  if (!data) {
+    return (
+      <Container>
+        <FlexCenter>
+          <div className="grid gap-5">
+            <Message msg="Looking for something tasty..." />
+          </div>
+        </FlexCenter>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <FlexCenter>
+        <div className="grid gap-5">
+          <RecipeCard data={data} />
+          <div className="flex justify-center gap-5">
+            <Button variant="outlined" onClick={() => console.log("save")}>
+              Save for later
+            </Button>
+            <Button onClick={() => Router.reload()}>Pick again</Button>
+          </div>
+        </div>
+      </FlexCenter>
+    </Container>
+  );
 };
 
 export default Random;
