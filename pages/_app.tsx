3bfef6deb7e268c4a2352ef5e5ce32451e9fe797
Carina <revuwem@gmail.com>
2022-08-17 01:13:45 +0400

    chore: remove manual auth configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,6 @@ import Layout from "../components/Layout";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const handleLogInResponse = (response: { credential: string }) => {
-    console.log(response.credential);
-  };
-
-  useEffect(() => {
-    /* global google */
-    // @ts-ignore
-    google.accounts.id.initialize({
-      client_id: process.env.NEXT_PUBLIC_GCP_CLIENT_ID,
-      callback: handleLogInResponse,
-    });
-
-    /* global google */
-    // @ts-ignore
-    google.accounts.id.renderButton(
-      document.getElementById("loginButton"),
-      { theme: "outline", size: "large" } // customization attributes
-    );
-  }, []);
-
   return (
     <Layout>
       <Component {...pageProps} />
