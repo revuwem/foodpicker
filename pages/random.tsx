0e42f9ceb1529673f4b3bb99d98a410aa3f3d980
Carina <revuwem@gmail.com>
2022-08-17 23:17:34 +0400

    feat: implement recipe saving

--- pages/random.tsx
+++ pages/random.tsx
@@ -7,12 +7,37 @@ import FlexCenter from "../components/FlexCenter";
 import Button from "../components/Button";
 import Router from "next/router";
 import Message from "../components/Message";
+import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
 
 const Random: NextPage = () => {
+  const { data: session } = useSession();
+
   const { data, error } = useSWR(
     `https://api.spoonacular.com/recipes/random?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}`,
     randomRecipeFetcher
   );
+
+  const [recipeSaved, setRecipeSaved] = useState<boolean>(false);
+
+  const onSaveRecipeClick = () => {
+    if (!session) {
+      signIn();
+    } else {
+      const localStorageSavedRecipes = localStorage.getItem("savedRecipes");
+
+      let savedRecipes: number[];
+      if (!localStorageSavedRecipes) {
+        savedRecipes = new Array();
+      } else {
+        savedRecipes = JSON.parse(localStorageSavedRecipes);
+      }
+
+      savedRecipes.push(data.id);
+      localStorage.setItem("savedRecipes", JSON.stringify(savedRecipes));
+      setRecipeSaved(true);
+    }
+  };
 
   if (error) {
     return (
@@ -44,8 +69,12 @@ const Random: NextPage = () => {
         <div className="grid gap-5">
           <RecipeCard data={data} />
           <div className="flex justify-center gap-5">
-            <Button variant="outlined" onClick={() => console.log("save")}>
-              Save for later
+            <Button
+              variant="outlined"
+              onClick={onSaveRecipeClick}
+              disabled={recipeSaved}
+            >
+              {!recipeSaved ? "Save for later" : "Recipe saved!"}
             </Button>
             <Button onClick={() => Router.reload()}>Pick again</Button>
           </div>
