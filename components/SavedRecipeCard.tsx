964550cb08afd6953a71b6102fc4cb4bdcc75537
Carina <revuwem@gmail.com>
2022-08-18 19:14:07 +0400

    feat: create SavedRecipeCard

---
+++ components/SavedRecipeCard.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import ExternalLinkIcon from "../assets/icons/external-link.svg";
+import soupImage from "../assets/images/soup.png";
+import Button from "./Button";
+
+const SavedRecipeCard = () => {
+  const onRemoveRecipeClick = () => {
+    console.log("remove");
+  };
+
+  return (
+    <div className="flex items-center gap-24">
+      <div className="flex gap-4">
+        <Image
+          src={soupImage}
+          layout="intrinsic"
+          width={100}
+          height={100}
+        ></Image>
+        <div className="flex flex-col">
+          <h3 className="text-md md:text-xl mb-auto">Recipe name</h3>
+          <p>Prep: 40 min</p>
+          <a href="" target="_blank" className="flex gap-1 hover:underline">
+            <ExternalLinkIcon />
+            Source link
+          </a>
+        </div>
+      </div>
+      <div className="flex gap-8">
+        <Button type="link" href="/">
+          Get recipe
+        </Button>
+        <Button variant="outlined" onClick={onRemoveRecipeClick}>
+          Remove
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SavedRecipeCard;
