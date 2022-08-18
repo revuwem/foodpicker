8344d58ff2f9a583b2d9655f5932a65418db79f4
Carina <revuwem@gmail.com>
2022-08-18 21:11:45 +0400

    feat: create saved recipes page

---
+++ pages/user/saved.tsx
@@ -0,0 +1,87 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import useSWR, { useSWRConfig } from "swr";
+import Button from "../../components/Button";
+import Container from "../../components/Container";
+import FlexCenter from "../../components/FlexCenter";
+import Message from "../../components/Message";
+import SavedRecipeCard, {
+  RecipeShrink,
+} from "../../components/SavedRecipeCard";
+import { recipeInformationBulkFetcher } from "../../utils/fetcher";
+
+const Saved: NextPage = () => {
+  const [localStorageSavedRecipes, setLocalStorageSavedRecipes] = useState<
+    number[]
+  >([]);
+  const [shouldFetch, setShouldFetch] = useState<boolean>(false);
+
+  useEffect(() => {
+    const savedRecipes = localStorage.getItem("savedRecipes") ?? "[]";
+    setLocalStorageSavedRecipes(JSON.parse(savedRecipes));
+    setShouldFetch(true);
+  }, []);
+
+  const { data, error } = useSWR(
+    shouldFetch && localStorageSavedRecipes.length > 0
+      ? `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&ids=${localStorageSavedRecipes}`
+      : null,
+    recipeInformationBulkFetcher
+  );
+
+  if (localStorageSavedRecipes.length === 0) {
+    return (
+      <Container>
+        <h2 className="font-bold text-2xl md:text-3xl text-green-100 mb-10">
+          Saved recipes
+        </h2>
+        <FlexCenter>
+          <Message msg="You don't have any recipes yet" />
+          <Button type="link" href="/random">
+            Pick random recipe
+          </Button>
+        </FlexCenter>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <h2 className="font-bold text-2xl md:text-3xl text-green-100 mb-10">
+          Saved recipes
+        </h2>
+        <p>Something went wrong</p>
+        <p>{error}</p>
+      </Container>
+    );
+  }
+
+  if (!data) {
+    return (
+      <Container>
+        <h2 className="font-bold text-2xl md:text-3xl text-green-100 mb-10">
+          Saved recipes
+        </h2>
+        <p>loading...</p>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <h2 className="font-bold text-2xl md:text-3xl text-green-100 mb-10">
+        Saved recipes
+      </h2>
+      <ul className="flex flex-col gap-16 md:gap-8">
+        {data.map((item: RecipeShrink) => (
+          <li>
+            <SavedRecipeCard data={item} />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default Saved;
--- utils/fetcher.tsx
+++ utils/fetcher.tsx
@@ -3,4 +3,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const randomRecipeFetcher = (url: string) =>
   fetcher(url).then((data) => data.recipes[0]);
 
-export { randomRecipeFetcher };
+const recipeInformationBulkFetcher = (url: string) =>
+  fetcher(url).then((data) => data);
+
+export { randomRecipeFetcher, recipeInformationBulkFetcher };
