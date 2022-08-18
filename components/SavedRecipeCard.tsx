8ba970ff7d75f217ef6343edf92abcea72af4b66
Carina <revuwem@gmail.com>
2022-08-18 21:11:13 +0400

    chore: render actual SaveRecipeCard data

--- components/SavedRecipeCard.tsx
+++ components/SavedRecipeCard.tsx
@@ -3,7 +3,19 @@ import ExternalLinkIcon from "../assets/icons/external-link.svg";
 import soupImage from "../assets/images/soup.png";
 import Button from "./Button";
 
-const SavedRecipeCard = () => {
+export type RecipeShrink = {
+  title: string;
+  image: string;
+  readyInMinutes: number;
+  sourceName: string;
+  sourceUrl: string;
+};
+
+interface SavedRecipeCardProps {
+  data: RecipeShrink;
+}
+
+const SavedRecipeCard = ({ data }: SavedRecipeCardProps) => {
   const onRemoveRecipeClick = () => {
     console.log("remove");
   };
@@ -12,17 +24,21 @@ const SavedRecipeCard = () => {
     <div className="flex flex-col md:flex-row md:items-center gap-8 md:gap-24">
       <div className="flex gap-4">
         <Image
-          src={soupImage}
+          src={data.image}
           layout="intrinsic"
           width={100}
           height={100}
         ></Image>
         <div className="flex flex-col text-sm">
-          <h3 className="text-xl mb-2">Recipe name</h3>
-          <p className="mb-auto">Prep: 40 min</p>
-          <a href="" target="_blank" className="flex gap-1 hover:underline">
+          <h3 className="text-xl mb-2">{data.title}</h3>
+          <p className="mb-auto">Prep: {data.readyInMinutes} min</p>
+          <a
+            href={data.sourceUrl}
+            target="_blank"
+            className="flex gap-1 hover:underline"
+          >
             <ExternalLinkIcon />
-            Source link
+            {data.sourceName}
           </a>
         </div>
       </div>
